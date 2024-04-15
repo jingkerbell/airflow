cfd963f854aea1f60fa75b0e265bd180fb90a08d
1100743 <>
2024-04-15 11:46:30 +0900

    plugins 폴더 추

---
+++ plugins/shell/select_fruit.sh
@@ -0,0 +1,11 @@
+FRUIT=$1
+if [ $FRUIT == APPLE ];then
+	echo "you selected Apple"
+elif [ $FRUIT == ORANGE ];then
+	echo "you selected Orange"
+elif [ $FRUIT == GRAPE ];then
+        echo "you selected Grape"
+else 
+	echo "you selected other Fruit"
+fi
+	
