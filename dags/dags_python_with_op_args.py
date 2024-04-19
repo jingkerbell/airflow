a9663605b49cdd82897e8c0b662a1192069a8261
1100743 <>
2024-04-19 10:00:12 +0900

    op_args 테스트용

---
+++ dags/dags_python_with_op_args.py
@@ -0,0 +1,20 @@
+from airflow import DAG
+import datetime
+import pendulum
+from airflow.operators.python import PythonOperator
+from common.common_func import regist
+
+with DAG(
+    dag_id="dags_python_task_decorator",
+    schedule="30 6 * * *",
+    start_date=pendulum.datetime(2024, 4, 18, tz="Asia/Seoul"),
+    catchup=False,
+) as dag:
+    
+    regist_t1 = PythonOperator(
+        task_id="regist_t1",
+        python_callable=regist,
+        op_args=['hjkim','man','kr','seoul']
+    )
+
+    regist_t1
