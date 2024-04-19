263d3acfa795f08dfa79cbb36797d8edc087edfc
1100743 <>
2024-04-19 13:57:33 +0900

    python template 업로드1

--- dags/dags_python_template.py
+++ dags/dags_python_template.py
@@ -6,7 +6,7 @@ from airflow.decorators import task
 
 
 with DAG(
-    dag_id="dags_bash_with_template",
+    dag_id="dags_python_template",
     schedule="10 0 * * *",
     start_date=pendulum.datetime(2024, 4, 9, tz="Asia/Seoul"),
     catchup=False
@@ -25,7 +25,7 @@ with DAG(
     def python_function2(**kwargs):
         print(kwargs)
         print('ds:'+ kwargs['ds'])
-        print('ts:'+ kwargs['ds'])
+        print('ts:'+ kwargs['ts'])
         print('data_interval_start:'+ str(kwargs['data_interval_start']))
         print('data_interval_end:'+ str(kwargs['data_interval_end']))
         print('task_instance:'+ str(kwargs['ti']))
